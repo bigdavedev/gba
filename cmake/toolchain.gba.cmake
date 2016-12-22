9f076f5401f43d0de560891c8ec3bd07ec56f5ec
Dave Brown <d.brown@bigdavedev.com>
2016-12-22 21:31:39 +0100

    Make toolchain file more robust
    Signed-off-by: Dave Brown <d.brown@bigdavedev.com>

--- cmake/toolchain.gba.cmake
+++ cmake/toolchain.gba.cmake
@@ -1,3 +1,5 @@
+set (DEVKITPRO_ROOT $ENV{DEVKITPRO_ROOT})
+
 if (DEVKITPRO_ROOT STREQUAL "")
 	message (FATAL "You need to set DEVKITPRO_ROOT to point to you devkitpro installation")
 endif()
@@ -5,9 +7,12 @@ endif()
 set (CMAKE_SYSTEM_NAME Generic)
 set (CMAKE_SYSTEM_PROCESSOR arm-none-eabi)
 
-set (CMAKE_C_COMPILER ${DEVKITPRO_ROOT}/bin/arm-none-eabi-gcc)
-set (CMAKE_CXX_COMPILER ${DEVKITPRO_ROOT}/bin/arm-none-eabi-g++)
-set (CMAKE_OBJCOPY ${DEVKITPRO_ROOT}/bin/arm-none-eabi-objcopy)
+set (CMAKE_FIND_ROOT_PATH ${DEVKITPRO_ROOT})
+
+find_program (CMAKE_OBJCOPY arm-none-eabi-objcopy)
+find_program (CMAKE_LINKER arm-none-eabi-ld)
+find_program (CMAKE_C_COMPILER arm-none-eabi-gcc)
+find_program (CMAKE_CXX_COMPILER arm-none-eabi-g++)
 
 set (ARCH "-mthumb-interwork -mthumb")
 set (SPECS -specs=gba.specs)
