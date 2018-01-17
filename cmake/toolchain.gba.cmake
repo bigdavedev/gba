f071e2d89d5ae8cc63c8d4e6126ab7820198104c
Dave Brown <d.brown@bigdavedev.com>
2018-01-17 10:32:43 +0100

    Backup commit
    Signed-off-by: Dave Brown <d.brown@bigdavedev.com>

--- cmake/toolchain.gba.cmake
+++ cmake/toolchain.gba.cmake
@@ -1,10 +1,9 @@
-
-if ("${DEVKITPRO_ROOT}" STREQUAL "")
-	message (FATAL "You need to set DEVKITPRO_ROOT to point to you devkitpro installation")
-endif()
+message (STATUS "devkitpro root directory: ${DEVKITPRO_ROOT}")
 
 set (CMAKE_SYSTEM_NAME Generic)
 set (CMAKE_SYSTEM_PROCESSOR arm-none-eabi)
+
+find_program(CMAKE_MAKE_PROGRAM make)
 
 set (CMAKE_FIND_ROOT_PATH ${DEVKITPRO_ROOT})
 
@@ -13,12 +12,20 @@ set (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set (CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set (CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
+find_program (CMAKE_C_COMPILER ${DEVKITPRO_ROOT}/bin/${CMAKE_SYSTEM_PROCESSOR}-gcc)
+find_program (CMAKE_CXX_COMPILER ${DEVKITPRO_ROOT}/bin/${CMAKE_SYSTEM_PROCESSOR}-g++)
+find_program (CMAKE_LINKER ${DEVKITPRO_ROOT}/bin/${CMAKE_SYSTEM_PROCESSOR}-ld)
+find_program (CMAKE_AR ${DEVKITPRO_ROOT}/bin/${CMAKE_SYSTEM_PROCESSOR}/ar)
+find_program (CMAKE_OBJCOPY ${DEVKITPRO_ROOT}/bin/${CMAKE_SYSTEM_PROCESSOR}-objcopy)
+find_program (CMAKE_STRIP ${DEVKITPRO_ROOT}/bin/${CMAKE_SYSTEM_PROCESSOR}-strip)
 
-find_program (CMAKE_OBJCOPY ${DEVKITPRO_ROOT}/bin/arm-none-eabi-objcopy)
-find_program (CMAKE_LINKER ${DEVKITPRO_ROOT}/bin/arm-none-eabi-ld)
-find_program (CMAKE_C_COMPILER ${DEVKITPRO_ROOT}/bin/arm-none-eabi-gcc)
-find_program (CMAKE_CXX_COMPILER ${DEVKITPRO_ROOT}/bin/arm-none-eabi-g++)
-find_program (CMAKE_STRIP ${DEVKITPRO_ROOT}/bin/arm-none-eabi-strip)
+message(STATUS "Looking for gbafix...")
+find_program (GBAFIX gbafix ${DEVKITPRO_ROOT}/bin)
+if(GBAFIX)
+	message(STATUS "Looking for gbafix: ${GBAFIX} - found")
+else(GBAFIX)
+	message(FATAL_ERROR "Looking for gbafix: ${GBAFIX} - not found")
+endif(GBAFIX)
 
 set (ARCH "-mthumb-interwork -mthumb")
 set (SPECS -specs=gba.specs)
--- test/main.cpp
+++ test/main.cpp
@@ -3,5 +3,10 @@
 int main(int argc, char* argv[])
 {
 	testing::InitGoogleMock(&argc, argv);
+
+	::testing::TestEventListeners& listeners =
+		      ::testing::UnitTest::GetInstance()->listeners();
+	delete listeners.Release(listeners.default_result_printer());
+	  listeners.Append(new ::testing::EmptyTestEventListener);
 	return RUN_ALL_TESTS();
 }
