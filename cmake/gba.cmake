caa4b3e4817ead701358c9f0b57d488d7606c2bc
Dave Brown <d.brown@bigdavedev.com>
2018-02-14 21:39:56 +0100

    No need to check for gbafix twice
    Signed-off-by: Dave Brown <d.brown@bigdavedev.com>

--- cmake/gba.cmake
+++ cmake/gba.cmake
@@ -1,13 +1,3 @@
-if(NOT GBAFIX)
-	message(STATUS "Looking for gbafix...")
-	find_program(GBAFIX gbafix ${DEVKITPRO_ROOT}/bin)
-	if(GBAFIX)
-		message(STATUS "Looking for gbafix: ${GBAFIX} - found")
-	else(GBAFIX)
-		message(FATAL_ERROR "Looking for gbafix: ${GBAFIX} - not found")
-	endif(GBAFIX)
-endif(NOT GBAFIX)
-
 macro(CONVERT_TO_GBA NAME)
 	add_custom_command(TARGET ${NAME}
 	                   POST_BUILD
